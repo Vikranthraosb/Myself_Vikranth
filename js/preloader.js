ce320f0ad14ddfc04db863fd1676927edd1b2c63
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-03-12 10:03:47 +0530

    almost fixed the preloader and is 90% responsive

--- js/preloader.js
+++ js/preloader.js
@@ -28,8 +28,8 @@ t1.from(".blr",{
     scale:.3,
     opacity:1,
     y:1000,
-    stagger:.20,
-    duration:.65,
+    stagger:.15,
+    duration:.55,
     borderRadius: "50",
     ease: Power3.easeInOut
 })
@@ -41,31 +41,32 @@ t1.to(".blr",{
    scale:1.2
 })
 t1.from(".topheading h5",{
-    x:150,
-    opacity:0,
-    stagger:.1,
-    duration:.6,
+    x: 150,
+    opacity: 0,
+    stagger: 0.1,
+    duration: 0.4,
     color: "green",
+    letterSpacing: "12px",
     ease: Power3.easeInOut
-    });
+});
 
 t1.from(".child span",{
 x:150,
 opacity:0,
 stagger:.19,
-duration:1.5,
+duration:1,
 color: "green",
 ease: Power3.easeInOut
 })
 
 t1.to(".parent .child", {
     y: "-100%", 
-    duration: .6,
+    duration: .7,
     ease: Power3.easeInOut
 });
 t1.to(".blr", {
     opacity: 0.85,
-    stagger: 0.17,
+    stagger: 0.12,
     duration: 0.2,
     borderRadius: "55%",
     scale:1.2
@@ -74,13 +75,13 @@ t1.to(".blr", {
 t1.to(".blr",{
     opacity:.6,
     y:-1000,
-    stagger:.1,
-    duration:.4,
+    stagger:.18,
+    duration:.3,
     ease: Power3.easeInOut
 })
 t1.to(".loader", {
     height:0,
-    duration: 1,
+    duration: .7,
     ease: Power3.easeInOut
 });
 
@@ -91,15 +92,15 @@ t1.to(".green", {
     "z-index": 999, 
     top: 0,
     left: 0,
-    delay: -0.70, 
-    duration: 1.5,
+    delay: -0.78, 
+    duration: 1.2,
     ease: Power3.easeInOut,
 });
 
 t1.to(".green", {
     height:"0%",
-    delay:-.23,
-    duration: .6,
+    delay:-.28,
+    duration: .5,
     ease: Power3.easeInOut
 });
 t1.eventCallback("onComplete", function() {
